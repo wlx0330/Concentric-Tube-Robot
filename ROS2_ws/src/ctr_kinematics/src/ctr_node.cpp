029bce912d35eb134e12c1ffc09105b7f91ea42f
Lexuan Wang <lwang848@uwo.ca>
2021-11-19 18:54:30 -0500

    blaze not working

--- ROS2_ws/src/ctr_kinematics/src/ctr_node.cpp
+++ ROS2_ws/src/ctr_kinematics/src/ctr_node.cpp
@@ -9,11 +9,15 @@ int main(int argc, char **argv)
   (void)argc;
   (void)argv;
 
-
-
-  Controller robot;
-  robot.ControllerMenu();
+  //Controller robot;
+  // robot.ControllerMenu();
+  blaze::StaticMatrix<double, 3UL, 3UL> temp;
+  temp = {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
   std::cout << 1 << std::endl;
+  std::cout << temp << std::endl;
+  system("pause");
+  std::cout << blaze::inv(temp) << std::endl;
+
   system("pause");
   return 0;
 }
